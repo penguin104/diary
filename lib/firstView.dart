7c3d6af6887113f6ebaeabc8d089fc1a49ac4e09
penguin104 <jd1908_0808@yahoo.co.jp>
2024-06-01 23:56:17 +0900

    firstView.dart view maked

--- lib/firstView.dart
+++ lib/firstView.dart
@@ -6,9 +6,64 @@ class firstViewWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final conName = Container(
+      width: 300,
+      child: TextField(
+        controller: TextEditingController(),
+        decoration: InputDecoration(
+          border: UnderlineInputBorder(),
+          labelText: "ユーザ名を入力",
+        ),
+      ),
+
+    );
+
+    void onpressed(){
+      debugPrint("start diary");
+    }
+
+    final conButton = Container(
+      alignment: Alignment(0.7,0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.teal, // background
+          foregroundColor: Colors.white //forgraund
+        ),
+        onPressed: onpressed,
+        child: Text("さぁ、始めよう!!"),
+        ),
+    );
+
+
+
+    final col = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Welcome Diary!",
+          style:TextStyle(
+            fontSize: 40
+          )
+        ),
+        conName,
+        conButton,
+      ],
+    );
+
+
+
     final v = Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text(
+          "Please Initial Registration",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),//タイトル
+      ),
       body: Center(
-        child: Text("diary"),
+        child: col,
       ),
     );
     return v;
