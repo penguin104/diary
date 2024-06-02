ef04985e65758f450f806b0d01c80a548db5dd84
penguin104 <jd1908_0808@yahoo.co.jp>
2024-06-02 13:02:04 +0900

    create home.dart home view write this file edit pubspec.yaml riverpod add

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+
+class homeView extends StatelessWidget {
+  const homeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const Placeholder();
+  }
+}
