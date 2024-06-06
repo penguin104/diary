04e4a4cd8ff288ab0fa530de6d49fd59b373a5bf
penguin104 <jd1908_0808@yahoo.co.jp>
2024-06-06 23:18:24 +0900

    create more.dart
    diaryModelを渡す

--- lib/home.dart
+++ lib/home.dart
@@ -1,33 +1,45 @@
 import 'package:flutter/material.dart';
 
-class diary{
+class diary {
   final String date;
   final String title;
   final String diaryText;
 
-  diary(this.date,this.title,this.diaryText);
+  diary(this.date, this.title, this.diaryText);
 }
 
-final List diaryModel = [//test model
+final List diaryModel = [
+  //test model
   diary("2024/6/2", "test", "ttttest"),
-  diary("2024/6/5", "penguin", "penguin"),  diary("2024/6/2", "test", "ttttest"),
   diary("2024/6/5", "penguin", "penguin"),
   diary("2024/6/2", "test", "ttttest"),
-  diary("2024/6/5", "penguin", "penguin"),  diary("2024/6/2", "test", "ttttest"),
-  diary("2024/6/5", "penguin", "penguin"),  diary("2024/6/2", "test", "ttttest"),
-  diary("2024/6/5", "penguin", "penguin"),  diary("2024/6/2", "test", "ttttest"),
-  diary("2024/6/5", "penguin", "penguin"),  diary("2024/6/2", "test", "ttttest"),
-  diary("2024/6/5", "penguin", "penguin"),  diary("2024/6/2", "test", "ttttest"),
-  diary("2024/6/5", "penguin", "penguin"),  diary("2024/6/2", "test", "ttttest"),
-  diary("2024/6/5", "penguin", "penguin"),  diary("2024/6/2", "test", "ttttest"),
-  diary("2024/6/5", "penguin", "penguin"),  diary("2024/6/2", "test", "ttttest"),
-  diary("2024/6/5", "penguin", "penguin"),  diary("2024/6/2", "test", "ttttest"),
-  diary("2024/6/5", "penguin", "penguin"),  diary("2024/6/2", "test", "ttttest"),
   diary("2024/6/5", "penguin", "penguin"),
+  diary("2024/6/2", "test", "ttttest"),
+  diary("2024/6/5", "penguin", "penguin"),
+  diary("2024/6/2", "test", "ttttest"),
+  diary("2024/6/5", "penguin", "penguin"),
+  diary("2024/6/2", "test", "ttttest"),
+  diary("2024/6/5", "penguin", "penguin"),
+  diary("2024/6/2", "test", "ttttest"),
+  diary("2024/6/5", "penguin", "penguin"),
+  diary("2024/6/2", "test", "ttttest"),
+  diary("2024/6/5", "penguin", "penguin"),
+  diary("2024/6/2", "test", "ttttest"),
+  diary("2024/6/5", "penguin", "penguin"),
+  diary("2024/6/2", "test", "ttttest"),
+  diary("2024/6/5", "penguin", "penguin"),
+  diary("2024/6/2", "test", "ttttest"),
+  diary("2024/6/5", "penguin", "penguin"),
+  diary("2024/6/2", "test", "ttttest"),
+  diary("2024/6/5", "penguin", "penguin"),
+  diary("2024/6/2", "test", "ttttest"),
+  diary("2024/6/5", "penguin", "penguin"),
+  diary("2024/6/2", "test", "ttttest"),
+  diary("2024/6/5", "penguin", "penguin"),
+]; //日記ウィジットをaddしていく
 
-];//日記ウィジットをaddしていく
-
-Widget modelToHomeDiaryWidget(diary diaryModel){//日記リストウィジェット
+Widget modelToHomeDiaryWidget(diary diaryModel) {
+  //日記リストウィジェット
   final conDate = Container(
     child: Text("${diaryModel.date}"),
   );
@@ -44,27 +56,23 @@ Widget modelToHomeDiaryWidget(diary diaryModel){//日記リストウィジェッ
     ),
   );
 
-
   final rowHomeDiaryTop = Row(
     children: [
       conDate,
     ],
   );
 
-  void moreDiary(){
+  void moreDiary() {
+    debugPrint(diaryModel.title); //diaryModelを次のページに渡す
     debugPrint("more");
   }
 
   final moreButton = ElevatedButton(
-
     onPressed: moreDiary,
     child: Text("さらに見る"),
     style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.cyan,
-        foregroundColor: Colors.white
-    ),
+        backgroundColor: Colors.cyan, foregroundColor: Colors.white),
   );
-
 
   final rowHomeDiaryButtom = Row(
     mainAxisAlignment: MainAxisAlignment.end,
@@ -76,8 +84,7 @@ Widget modelToHomeDiaryWidget(diary diaryModel){//日記リストウィジェッ
 
   final conRow = Container(
     padding: EdgeInsets.all(10),
-
-    child:rowHomeDiaryButtom,
+    child: rowHomeDiaryButtom,
   );
 
   final colHomeDiary = Column(
@@ -100,51 +107,41 @@ class homeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     final listViewDiary = ListView.builder(
       itemCount: diaryModel.length,
       itemBuilder: (c, i) => modelToHomeDiaryWidget(diaryModel[i]),
     );
 
-
     final conDiary = Container(
-      height: 500,
+      height: 700,
       width: double.infinity,
       color: Colors.black12,
-      child: listViewDiary,//diaryList
+      child: listViewDiary, //diaryList
     );
 
-
-
-    void onPressed(){
+    void addDiary() {
       debugPrint("addDiary");
     }
 
-    final addDiaryButton = IconButton(
-      onPressed: onPressed,
-      style: IconButton.styleFrom(
-        backgroundColor: Colors.teal,
-        foregroundColor: Colors.white,
-      ),
-      iconSize: 50,
-      icon: Icon(
-        Icons.add,
-      ),
-    );
-
     final conAddButton = Container(
-      height: 100,
-      alignment: Alignment(0.8,1),
-      child: addDiaryButton,
-    );
-
+        width: 80,
+        height: 80,
+        child: FloatingActionButton(
+          onPressed: () {
+            addDiary();
+          },
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.teal,
+          child: const Icon(
+            Icons.add,
+            size: 30,
+          ),
+        ));
 
     final col = Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         conDiary,
-        conAddButton,
       ],
     );
 
@@ -154,14 +151,15 @@ class homeView extends StatelessWidget {
         title: Text(
           "Home",
           style: TextStyle(
-            color: Colors.white
-            ,
+            color: Colors.white,
           ),
         ),
       ),
-      body : Center(
+      body: Center(
         child: col,
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: conAddButton,
     );
     return homeViewWidget;
   }
---
+++ lib/more.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class moreView extends StatelessWidget {
+  const moreView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final sca = Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text(),
+      ),
+    );
+
+    final conSca = Container();
+
+    return const Placeholder();
+  }
+}
